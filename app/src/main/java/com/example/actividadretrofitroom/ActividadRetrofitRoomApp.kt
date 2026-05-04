7c788bcdd1dd147c2b25ee91af1493c2650a6bef
Andres Eduardo Perez <andrese.perezm@uqvirtual.edu.co>
2026-05-04 09:05:07 -0500

    Dependencies applied

---
+++ app/src/main/java/com/example/actividadretrofitroom/ActividadRetrofitRoomApp.kt
@@ -0,0 +1,7 @@
+package com.example.actividadretrofitroom
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ActividadRetrofitRoomApp : Application()
